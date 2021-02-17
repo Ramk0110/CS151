c27a0f3f289dcadb1eeaa482b6fa6608c106c704
Ram Khanna <ram.khanna@sjsu.edu>
2021-02-16 21:25:36 -0800

    Added javadocs

--- Stock.java
+++ Stock.java
@@ -2,21 +2,26 @@ package hw1;
 
 public class Stock 
 {
-	 private String stock;
-	
+	/**Creates instance variable
+	 * 
+	 */
+	private String stock;
+	 /** Creates an stock with the specified name.
+	  * @param f name of the stock
+	 */
 	public Stock(String f)
 	{
 		 this.stock=f;
     }
-
+	/**
+	 * @return name of the stock
+	 */
 	public String getStock() 
 	{
 		return stock;
 	}
 	
 
-	
-	
 
 	
 }
--- StockTester.java
+++ StockTester.java
@@ -1,7 +1,13 @@
 package hw1;
 
+/** This is a tester class for Stock.java
+ *
+*/
 public class StockTester 
 {
+	/** This is the main method 
+	 * prints the name of the stock
+	*/
 	public static void main(String[] args)
 	{
 		Stock stock=new Stock("Tesla");
